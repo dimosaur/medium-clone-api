cff7a8c12ba939c7435dade4b1837a4ac7f97e36
Dmitry <civil.dinosaur@gmail.com>
2021-12-18 01:21:09 +0400

    Initialize

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+if (process.env.IS_TS) {
+  require('module-alias/register')
+}
+
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from '@app/app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000)
 }
-bootstrap();
+
+bootstrap()
