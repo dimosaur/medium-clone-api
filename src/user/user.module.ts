72e1181384582627ed4ad518d8d67a57133bee5d
Dmitry Kochnev <civil.dinosaur@gmail.com>
2022-01-03 13:46:55 +0400

    [fix][user] - remove unnecessary declaration of provider

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ConfigModule } from '@nestjs/config'
 
-import { AuthGuard } from '@app/user/guards/auth.guard'
 import { UserController } from '@app/user/user.controller'
 import { UserService } from '@app/user/user.service'
 import { UserEntity } from '@app/user/user.entity'
@@ -10,7 +9,7 @@ import { UserEntity } from '@app/user/user.entity'
 @Module({
   imports: [ConfigModule, TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
-  providers: [UserService, AuthGuard],
+  providers: [UserService],
   exports: [UserService],
 })
 export class UserModule {}
